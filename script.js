91832e76ba69c6634793e4e6397d0866ead7b565
macielegaspi <37843512+macielegaspi@users.noreply.github.com>
2019-02-15 15:41:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+
+
+function letterN(el){
+	document.getElementById("l1").src = "images/letterN.png";
+}
+
+function letterA(el){
+	document.getElementById("l2").src = "images/letterA.png";
+}
+
+function letterS(el){
+	document.getElementById("l3").src = "images/letterS.png";
+}
+
+function letterT(el){
+	document.getElementById("l4").src = "images/letterT.png";
+}
+
+function letterL(el){
+	document.getElementById("l5").src = "images/letterL.png";
+}
+
+function letterI(el){
+	document.getElementById("l6").src = "images/letterI.png";
+}
+
